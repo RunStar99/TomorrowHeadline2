b33dd0314980a15c3ab32c59e10ba1242208bf15
fly <986472617@qq.com>
2019-06-11 14:30:32 +0800

    界面初步完成

--- app/src/main/java/com/example/tomorrowheadline/MainActivity.java
+++ app/src/main/java/com/example/tomorrowheadline/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.tomorrowheadline;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.bottom_app_bar,menu);
+        return true;
+    }
+
 }
